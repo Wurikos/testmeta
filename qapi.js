c0ba8903c1509be12054934b6a02bca9c53f449b
Wurikos <50768680+Wurikos@users.noreply.github.com>
2021-01-29 13:06:39 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,40 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"6013ddbf10485c001e656972",
+				"6013ddbf10485c001e656974"
+			]
+		},
+		"6013ddbf10485c001e656972": {
+			"id": "6013ddbf10485c001e656972",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"6013ddbf10485c001e656974": {
+			"id": "6013ddbf10485c001e656974",
+			"name": "index",
+			"pageUrl": "index",
+			"customCode": {
+				"6013ddd2a64e2e91a91f8a6f": {
+					"name": "meta",
+					"place": "endOfHead",
+					"tag": "meta",
+					"data": {
+						"name": "keywords",
+						"content": "цена купить дешево слова ключевые"
+					}
+				}
+			}
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
